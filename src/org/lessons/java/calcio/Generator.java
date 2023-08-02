50b51f9eb932a429b46bbe08d17166b1aeac003c
DavideMinutoli <noprivacyworld@gmail.com>
2023-08-02 18:03:07 +0200

    creation map for role player by strategy

--- src/org/lessons/java/calcio/Generator.java
+++ src/org/lessons/java/calcio/Generator.java
@@ -1,8 +1,6 @@
 package org.lessons.java.calcio;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class Generator {
     private static final ArrayList<String> nameContainer = new ArrayList<>(Arrays.asList("Marco Rossi", "Alessio Bianchi", "Lorenzo Russo", "Simone Ferrara", "Andrea Martini",
@@ -26,32 +24,66 @@ public class Generator {
             "Angelo Ruggiero", "Carmine Sorrentino", "Emanuele Farina", "Francesco Rinaldi",
             "Luigi Esposito", "Domenico Vitale", "Antonio De Angelis", "Mario Barbieri", "Giovanni Marchetti",
             "Francesco Russo", "Raffaele Martini", "Federico Coppola", "Emanuele Bellini", "Alessio Ricci"));
-    private static String namePlayer;
+
     private static final ArrayList<String> nameCoachContainer = new ArrayList<>(Arrays.asList("John Smith", "Maria Garcia", "Robert Johnson", "Emily Martinez", "Michael Brown",
             "Jessica Davis", "William Taylor", "Jennifer Anderson", "David Wilson", "Linda Martinez",
             "James Thompson", "Patricia Rodriguez", "Daniel White", "Susan Lee", "Charles Harris",
             "Karen Miller", "Matthew Moore", "Nancy Taylor", "Andrew Davis", "Margaret Martinez",
             "Joseph Clark", "Sarah Jones", "Christopher Martinez", "Lisa Taylor", "Thomas Anderson",
             "Betty Williams", "Daniel Martin", "Linda Thompson", "Paul Harris", "Donna Johnson"));
-    private static String nameCoach;
-    private static final ArrayList<String> roleContainer = new ArrayList<>(Arrays.asList("Portiere", "Difensore", "Centrocampista", "Attaccante centrale"));
-    private static String rolePlayer;
     private static final ArrayList<String> strategyContainer = new ArrayList<>(Arrays.asList("4-4-2", "4-3-3", "3-5-2", "4-2-3-1",
             "5-3-2", "4-1-4-1", "4-5-1", "3-4-3", "4-3-1-2", "3-4-2-1",
             "3-3-3-1", "5-2-2-1", "4-2-2-2", "3-4-1-2", "5-4-1",
             "3-4-3", "4-2-4", "4-2-2-2", "4-1-2-1-2", "4-3-2-1",
             "4-4-1-1", "3-6-1", "5-2-3", "3-3-1-3", "3-4-1-2",
             "3-5-1-1", "3-3-2-2", "5-3-1-1", "4-3-1-2", "4-3-3-1"));
+    private static final ArrayList<String> roleContainer = new ArrayList<>(Arrays.asList("Portiere", "Difensore", "Centrocampista", "Attaccante"));
+    private static final Map<String, int[]> strategyToRolesMap = new HashMap<>();
+    static {
+        strategyToRolesMap.put("4-4-2", new int[]{4, 4, 2});
+        strategyToRolesMap.put("4-3-3", new int[]{4, 3, 3});
+        strategyToRolesMap.put("3-5-2", new int[]{3, 5, 2});
+        strategyToRolesMap.put("4-2-3-1", new int[]{4, 2, 4});
+        strategyToRolesMap.put("5-3-2", new int[]{5, 3, 2});
+        strategyToRolesMap.put("4-1-4-1", new int[]{4, 1, 5});
+        strategyToRolesMap.put("4-5-1", new int[]{4, 5, 1});
+        strategyToRolesMap.put("3-4-3", new int[]{3, 4, 3});
+        strategyToRolesMap.put("4-3-1-2", new int[]{4, 3, 3});
+        strategyToRolesMap.put("3-4-2-1", new int[]{3, 4, 3});
+        strategyToRolesMap.put("3-3-3-1", new int[]{3, 3, 4});
+        strategyToRolesMap.put("5-2-2-1", new int[]{5, 2, 3});
+        strategyToRolesMap.put("4-2-2-2", new int[]{4, 2, 4});
+        strategyToRolesMap.put("3-4-1-2", new int[]{3, 4, 3});
+        strategyToRolesMap.put("5-4-1", new int[]{5, 4, 1});
+        strategyToRolesMap.put("3-4-3", new int[]{3, 4, 3});
+        strategyToRolesMap.put("4-2-4", new int[]{4, 2, 4});
+        strategyToRolesMap.put("4-2-2-2", new int[]{4, 2, 4});
+        strategyToRolesMap.put("4-1-2-1-2", new int[]{4, 3, 3});
+        strategyToRolesMap.put("4-3-2-1", new int[]{4, 3, 3});
+        strategyToRolesMap.put("4-4-1-1", new int[]{4, 4, 2});
+        strategyToRolesMap.put("3-6-1", new int[]{3, 6, 1});
+        strategyToRolesMap.put("5-2-3", new int[]{5, 2, 3});
+        strategyToRolesMap.put("3-3-1-3", new int[]{3, 3, 4});
+        strategyToRolesMap.put("3-4-1-2", new int[]{3, 4, 3});
+        strategyToRolesMap.put("3-5-1-1", new int[]{3, 5, 2});
+        strategyToRolesMap.put("3-3-2-2", new int[]{3, 3, 4});
+        strategyToRolesMap.put("5-3-1-1", new int[]{5, 3, 2});
+        strategyToRolesMap.put("4-3-1-2", new int[]{4, 3, 3});
+        strategyToRolesMap.put("4-3-3-1", new int[]{4, 3, 4});
+    }
+    private static final Random random = new Random();
+    private static String nameCoach;
+    private static String namePlayer;
+    private static String rolePlayer;
     private static String strategy;
     private static final ArrayList<String> usedNames = new ArrayList<>();
     private static final ArrayList<String> usedNamesCoach = new ArrayList<>();
-    private static final Random random = new Random();
+
 
 
     //methods
     public static Player generatePlayer() {
         generateNameRandom();
-        generateRoleRandom();
         return new Player(namePlayer, rolePlayer);
     }
     public static Coach generateCoach() {
@@ -91,12 +123,6 @@ public class Generator {
             usedNamesCoach.add(nameCoach );
             return nameCoach;
         }
-        //generation role random TEST
-        private static String generateRoleRandom() {
-            int indexRandom;
-            indexRandom = random.nextInt(roleContainer.size());
-            rolePlayer = roleContainer.get(indexRandom);;
-            return rolePlayer;
-        }
+
 
 }
--- src/org/lessons/java/calcio/SoccerTeam.java
+++ src/org/lessons/java/calcio/SoccerTeam.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public class SoccerTeam {
     //VARIABLES
     private String coach;
+    private String goalKeeper;
     private ArrayList<Player> soccerTeam;
+
     //CONSTRUCTOR
-    public SoccerTeam(String coach, ArrayList<Player> soccerTeam) {
+    public SoccerTeam(String coach, String goalKeeper, ArrayList<Player> soccerTeam) {
         this.coach = coach;
+        this.goalKeeper = goalKeeper;
         this.soccerTeam = soccerTeam;
     }
+
     //getter & setter
         //get
         public String getCoach() {
@@ -19,6 +23,9 @@ public class SoccerTeam {
         public ArrayList<Player> getSoccerTeam() {
             return soccerTeam;
         }
+        public String getGoalKeeper() {
+            return goalKeeper;
+        }
         //set
         public void setCoach(String coach) {
             this.coach = coach;
@@ -26,6 +33,10 @@ public class SoccerTeam {
         public void setSoccerTeam(ArrayList<Player> soccerTeam) {
             this.soccerTeam = soccerTeam;
         }
+        public void setGoalKeeper(String goalKeeper) {
+            this.goalKeeper = goalKeeper;
+        }
+
     //toString
     @Override
     public String toString() {
